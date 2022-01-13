31cd6cc6c5f4f97ea6788a35168ce7339c966d99
INMD1 <lyw514549@gmail.com>
2022-01-14 01:39:12 +0900

    main page image covert wedp

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
   <article style="  background: linear-gradient(to right, #eacda3, #d6ae7b);">
     <div class="py-5 h-100 align-items-center d-flex">
       <div class="hero mx-auto">
-          <img :src="require('./assets/img/20220113_200043.jpg')">
+          <img :src="require('./assets/img/20220113_200043.webp')">
           <p class="hello">Hello world</p>
           <p class="hello_body">여러분들을 만나서 반가워요. 저에 대해서 한번 알아보까요?</p>
       </div>
