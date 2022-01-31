5846aa78a04820cf6f86735e607433af28d9aa41
INMD1 <lyw514549@gmail.com>
2022-02-01 00:31:47 +0900

    About Renewal at 50%

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <nav class="navbar navbar-expand-lg fixed-top" v-bind:style="{background: topcolor, transition: 'all ease 0.3s 0s' }">
+  <nav class="navbar navbar-expand-lg fixed-top navbar-light" v-bind:style="{background: topcolor, transition: 'all ease 0.3s 0s' }">
     <div class="container">
       <a class="navbar-brand title" href="#">INMD1</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <div class="navbar-nav">
         <a class="nav-item nav-link menu" href="#">Home</a>
         <a class="nav-item nav-link menu" href="#">About</a>
         <a class="nav-item nav-link menu" href="#">Skill</a>
         <a class="nav-item nav-link menu" href="#">project</a>
       </div>
+      </div>
     </div>
   </nav>
-  <article style="background-color: #9AD0EC;">
+  <article style="background-color: #D3DEDC;">
     <div class="py-5 h-100 align-items-center text-center d-flex img-fluid">
       <div class="container">
           <div class="row">
               <div class="col">
                   <p class="title_B">안녕하세요,</p>
-                  <p class="title_B">저는 <span style="color: #1572A1;">INMD1</span> 이고</p>
-                  <p class="title_B">개발자를 <span style="color: #1572A1;">꿈꾸는 </span>사람 입니다.</p>
+                  <p class="title_B">저는 <span style="color: #22577E;">INMD1</span> 이고</p>
+                  <p class="title_B"><span style="color: #064635;">개발자</span>를 <span style="color: #1572A1;">꿈꾸는 </span>사람 입니다.</p>
               </div>
               <div class="col-lg-5"><img class="rounded-circle" :src="require('./assets/img/icon.png')" style="width: 30vh; filter: drop-shadow(5px 5px 5px #000);" /></div>
           </div>
