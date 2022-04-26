18f2003b0289595484b4987853c9de34df08359b
INMD1 <87979171+INMD1@users.noreply.github.com>
2022-04-26 19:12:32 +0900

    update

--- src/components/more.vue
+++ src/components/more.vue
@@ -13,15 +13,15 @@
           <div class="row">
             <div class="col-md-4 col-lg-5 botton">
               <div class="card more_card">
-                <div class="card-body card_center" >
-                  <p class="card-title" style="font-size: 50px ; font-weight: Bold;"><i class="bi bi-github"></i> Github</p>
+                <div class="card-body" style="text-align: center;">
+                  <p class="card-title card_title-e"><i class="bi bi-github"></i> Github</p>
                   <h6 class="text-muted card-subtitle mb-2">
                     <a href="https://github.com/INMD1"
                       >https://github.com/INMD1</a
                     >
                   </h6>
                   <p class="card-text">이때 까지 만든 코드를 올리는 사이트</p>
-                  <ul>
+                  <ul class="ul-col-center">
                     <li>CLI 프로그램 소스코드</li>
                     <li>학교 실습 소스코드</li>
                     <li>웹앱 프로그램 소스코드</li>
@@ -33,8 +33,8 @@
             <div class="col-md-3 col-lg-2 col-xl-1"></div>
             <div class="col-md-4 col-lg-5 botton">
               <div class="card more_card">
-                <div class="card-body card_center">
-                  <p class="card-title" style="font-size: 50px ; font-weight: Bold;"><i class="bi bi-archive"></i> Amview</p>
+                <div class="card-body" style="text-align: center;">
+                  <p class="card-title card_title-e"><i class="bi bi-archive"></i> Amview</p>
                   <h6 class="text-muted card-subtitle mb-2">
                     <a href="https://amview.powerinmd.com"
                       >https://amview.powerinmd.com</a
@@ -43,7 +43,7 @@
                   <p class="card-text">
                     코딩 문제를 풀고 소스코드를 공개하는 사이트
                   </p>
-                  <ul>
+                  <ul class="ul-col-center">
                     <li>백준 문제 소스코드</li>
                     <li>라비다 문제 소스코드</li>
                     <li>Ascode 문제 소스코드</li>
