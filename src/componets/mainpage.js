2c10670ac30a72ee6935c683bf677a9b831a438a
INMD1 <lyw514549@gmail.com>
2024-03-21 21:13:06 +0900

    중간 업데이트

--- src/componets/mainpage.js
+++ src/componets/mainpage.js
@@ -3,10 +3,10 @@ import { Grid, GridItem, Box, Image, Center } from "@chakra-ui/react";
 function mainpage() {
     return (
         //반응형 적용
-        <div class="grid lg:grid-rows-3 md:grid-flow-col gap-4" style={{paddingTop: "10vh", height: "90vh"}}>
+        <div class="grid lg:grid-rows-3 md:grid-flow-col gap-4" style={{paddingTop: "8vh", height: "99vh"}}>
             <div class="row-span-3 border-2 bg-sky-500">1번째 레이어</div>
-            <div class="border-2 bg-sky-500">2번째 레이어</div>
-            <div class="row-span-2  bg-sky-500">3번째 레이어</div>
+            <div class="row-span-2 border-2 bg-sky-500">2번째 레이어</div>
+            <div class="bg-sky-500">3번째 레이어</div>
         </div>
     )
 }
