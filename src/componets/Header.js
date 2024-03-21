ce04cb5d017dcfe6ee0236981a89c0236e9f3516
INMD1 <lyw514549@gmail.com>
2024-03-21 23:44:18 +0900

    asd

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -8,7 +8,7 @@ const NavBar = () => {
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
 
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap">Site Name</span>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap">INMD1의 나른한곳</span>
                     </a>
                     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <Button size='lg' variant='link' leftIcon={<FaGithub style={{ height: '3.5vh', width: 'auto' }} color='black' />} />
@@ -23,16 +23,16 @@ const NavBar = () => {
                     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
                             <li>
-                                <a href="#" class="block py-2 px-3 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+                                <a href="#" class="block py-2 px-3 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home&About</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Skill</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Project</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">More?</a>
                             </li>
                         </ul>
                     </div>
--- src/componets/mainpage.js
+++ src/componets/mainpage.js
@@ -1,16 +1,18 @@
-import { Center, Image } from '@chakra-ui/react'
+import { Center, Heading, Image } from '@chakra-ui/react'
 
 function mainpage() {
     return (
         //반응형 적용
         <div class="grid lg:grid-rows-3 md:grid-flow-col gap-4" style={{ padding: "10%", backgroundColor: "#F8F1F1" }}>
-            <div class="lg:row-span-3 border-2 bg-sky-500"></div>
-            <div class="lg:row-span-2">
+            <div class="lg:row-span-3 col-span-3 p-10" style={{borderRadius: "5%", backgroundColor: "#9BB8CD"}}>
+                <Heading size='2xl' >Hello World!</Heading>
+            </div>
+            <div class="lg:row-span-2 col-span-2" >
                 <Center>
-                    <Image style={{borderRadius: "50%"}} src='https://avatars.githubusercontent.com/u/87979171?v=4'/>
+                    <Image style={{borderRadius: "50%", height: "40vh"}} src='https://avatars.githubusercontent.com/u/87979171?v=4'/>
                 </Center>
             </div>
-            <div class="bg-sky-500">3번째 레이어</div>
+            <div class="col-span-2 border-2" >3번째 레이어</div>
         </div>
     )
 }
