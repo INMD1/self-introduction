1cb34876ea205e6279f787435687ea810da62c5a
INMD1 <lyw514549@gmail.com>
2024-03-21 22:54:21 +0900

    체크 포인트

--- src/componets/mainpage.js
+++ src/componets/mainpage.js
@@ -1,11 +1,15 @@
-import { Grid, GridItem, Box, Image, Center } from "@chakra-ui/react";
+import { Center, Image } from '@chakra-ui/react'
 
 function mainpage() {
     return (
         //반응형 적용
-        <div class="grid lg:grid-rows-3 md:grid-flow-col gap-4" style={{paddingTop: "8vh", height: "99vh"}}>
-            <div class="row-span-3 border-2 bg-sky-500">1번째 레이어</div>
-            <div class="row-span-2 border-2 bg-sky-500">2번째 레이어</div>
+        <div class="grid lg:grid-rows-3 md:grid-flow-col gap-4" style={{ padding: "10%", backgroundColor: "#F8F1F1" }}>
+            <div class="lg:row-span-3 border-2 bg-sky-500"></div>
+            <div class="lg:row-span-2">
+                <Center>
+                    <Image style={{borderRadius: "50%"}} src='https://avatars.githubusercontent.com/u/87979171?v=4'/>
+                </Center>
+            </div>
             <div class="bg-sky-500">3번째 레이어</div>
         </div>
     )
