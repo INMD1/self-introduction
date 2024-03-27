29d6c2fa28f51f9276d6606a1a6fecbaee2f827a
INMD1 <lyw514549@gmail.com>
2024-03-27 20:30:39 +0900

    사진 고민 할것

--- src/componets/project.js
+++ src/componets/project.js
@@ -1,14 +1,14 @@
-import { Card, CardHeader, CardBody, CardFooter, Text, Image, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Text, Image, Tabs, TabList, TabPanels, Tab, TabPanel, Heading } from '@chakra-ui/react'
 
 const Project = () => {
     return (
-        <div className="h-full" style={{ padding: "10%", paddingTop: "25vh", backgroundColor: "#548CA8" }}>
+        <div className="h-full" style={{ padding: "5%", paddingTop: "25vh", backgroundColor: "#548CA8" }}>
             <Card>
                 <CardBody>
                     <div className='grid gap-10 grid-rows-2 sm:grid-cols-2 sm:grid-rows-1'>
-                        <Image src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'></Image>
+                        <Image src='/image/1p.webp'></Image>
                         <div>
-                            <Tabs variant='enclosed'>
+                            <Tabs>
                                 <TabList>
                                     <Tab>1</Tab>
                                     <Tab>2</Tab>
@@ -17,14 +17,19 @@ const Project = () => {
                                 </TabList>
                                 <TabPanels>
                                     <TabPanel>
-                                        <p>one!</p>
+                                        <Heading size="lg">DEU-Widget</Heading>
+                                        <br/>
+                                        <Text fontSize="md">
+                                            이 프로젝트는 동의대학교 여러가지 앱에서 자주 이용하는 앱하고 학식을 스마트폰 메인페이지에서 먼저 보이게 하면 어떨까
+                                            해서 만들었습니다. 동의모바일하고 동의대출결앱을 바로 갈수 있게 버튼을 만들고 학식을 볼수있게 리스트를 만들었습니다.
+                                        </Text>
                                     </TabPanel>
                                     <TabPanel>
                                         <p>two!</p>
                                     </TabPanel>
                                 </TabPanels>
+                                <Text fontSize="xs">좀더 보고싶으면 깃허브를 방문해주세요!</Text>
                             </Tabs>
-                            <Text fontSize="xs">좀더 보고싶으면 깃허브를 방문해주세요!</Text>
                         </div>
                     </div>
                 </CardBody>
