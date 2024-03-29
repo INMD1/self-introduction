4657240e1500b9ae8203673ac1a99b7ce850b22b
INMD1 <lyw514549@gmail.com>
2024-03-29 16:01:18 +0900

    업로드 중

--- src/componets/project.js
+++ src/componets/project.js
@@ -1,19 +1,32 @@
 import { Text, Image, Stack, VStack, Heading, Center, Button } from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { useState } from 'react';
+import jsondata from './data/index.json'
+
 const Project = () => {
+    const [data, setData] = useState({
+        "title": "None",
+        "body": "프로젝트를 선택해주세요.",
+        "imageurl": "/image/403.webp",
+        "size": "45%"
+    }); 
+
+    function changedata(number) {
+        setData(jsondata[number])
+    }
+
     return (
         <div className='h-full p-10 sm:p-72' style={{ backgroundColor: "#EEF5FF" }}>
-            <div className='flex grid gap-0 grid-rows-2 sm:grid-cols-3 sm:grid-rows-2 sm:gap-16'>
-                <div className='row-span-2 col-span-3 pt-5 content-center sm:col-span-2 sm:pt-0' style={{ backgroundColor: "#B4D4FF", borderRadius: "5%" }}>
-                    <Image height="32%" src='/image/1p.webp' style={{ margin: "auto" }} ></Image>
+            <div className='flex grid gap-0 pt-24 grid-rows-2 sm:grid-cols-3 sm:grid-rows-2 sm:gap-16 sm:pt-0'>
+                <div className='row-span-2 col-span-3 pt-10 content-center sm:col-span-2 sm:pt-0' style={{ backgroundColor: "#B4D4FF", borderRadius: "5%" }}>
+                    <Image height={data.size} src={data.imageurl} style={{ margin: "auto" }} ></Image>
                     <br />
                     <Stack direction='row'>
                         <div className='mx-0 p-5 sm:mx-20' >
-                            <Heading size="lg">DEU-Widget</Heading>
+                            <Heading size="lg">{data.title}</Heading>
                             <br />
                             <Text className='' fontSize="md">
-                                이 프로젝트는 동의대학교 여러가지 앱에서 자주 이용하는 앱하고 학식을 스마트폰 메인페이지에서 먼저 보이게 하면 어떨까
-                                해서 만들었습니다. 동의모바일하고 동의대출결앱을 바로 갈수 있게 버튼을 만들고 학식을 볼수있게 리스트를 만들었습니다.
+                                {data.body}
                             </Text>
                         </div>
                     </Stack>
@@ -24,7 +37,7 @@ const Project = () => {
                     <br />
                     <Tabs isLazy>
                         <TabList>
-                            <Tab>Web&*Webapp</Tab>
+                            <Tab>Web/Webapp</Tab>
                             <Tab>Android</Tab>
                             <Tab>Api</Tab>
                         </TabList>
@@ -32,23 +45,23 @@ const Project = () => {
                         <TabPanels>
                             <TabPanel>
                                 <VStack align='stretch'>
-                                    <Button>self-introduction</Button>
-                                    <Button>APSU</Button>
-                                    <Button>Algorithm-web-view</Button>
-                                    <Button>server-dashboard</Button>
-                                    <Button>DoD_DashBoard</Button>
+                                    <Button onClick={() => {changedata(0)}}>self-introduction</Button>
+                                    <Button onClick={() => {changedata(1)}}>APSU</Button>
+                                    <Button onClick={() => {changedata(0)}}>Algorithm-web-view</Button>
+                                    <Button onClick={() => {changedata(0)}}>server-dashboard</Button>
+                                    <Button onClick={() => {changedata(0)}}>DoD_DashBoard</Button>
                                 </VStack>
                             </TabPanel>
                             <TabPanel>
                                 <VStack align='stretch'>
-                                    <Button>deu_food_gui-widget-only </Button>
+                                    <Button onClick={() => {changedata(0)}}>deu_food_gui-widget-only </Button>
                                 </VStack>
                             </TabPanel>
                             <TabPanel>
                                 <VStack align='stretch'>
-                                    <Button>Computer-API</Button>
-                                    <Button>deu-Dorm-meal</Button>
-                                    <Button>Deu_food_api</Button>
+                                    <Button onClick={() => {changedata(0)}}>Computer-API</Button>
+                                    <Button onClick={() => {changedata(0)}}>deu-Dorm-meal</Button>
+                                    <Button onClick={() => {changedata(0)}}>Deu_food_api</Button>
                                 </VStack>
                             </TabPanel>
                         </TabPanels>
